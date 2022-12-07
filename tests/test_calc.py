d56b330f90abb821088569e31b8ab9fd3bf957c5
Vasilka69 <114226684+Vasilka69@users.noreply.github.com>
2022-12-08 03:41:33 +0500

    pytests

---
+++ tests/test_calc.py
@@ -0,0 +1,14 @@
+import src.main as main
+import pytest
+
+# Тест функции, которая производит математические операции
+class TestCalc:
+
+    # Тестируем программу на коррктных данных.
+    def test_on_correct_sign(self):
+        assert main.calc(3, 5, '*') == 15
+
+    # Тестируем программу на некорретных данных. Функция вызывает исключение ZeroDivisionError.
+    def test_on_div_by_zero(self):
+        with pytest.raises(ZeroDivisionError):
+            main.calc(3, 0, '/')
